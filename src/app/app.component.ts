6a783d1c81923b6628bf1d90d42ad128f403c5f6
Glenn Pinto <gvpinto@gmail.com>
2022-01-02 00:03:47 -0500

    256. Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,14 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http
+      .post(
+        'https://ng-complete-guide-b04f9-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
